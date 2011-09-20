d64f5a9cd853d1b2c15288a4bf368cc044d594eb
Szymon Witamborski <santamon@gmail.com>
2011-09-20 19:59:20 +0200

    Refactor, readded media query.

--- sroo.js
+++ sroo.js
@@ -4,29 +4,32 @@
   Copywrong 2011 Szymon Witamborski
   http://longstandingbug.com/info.html
 */
-(function() {
+/*jslint browser: true, vars: true, white: true */
+/*global requestAnimationFrame*/
+(function () {
+    "use strict";
     var body = document.body;
     var slides = [];
-    var slide, node, nodeName, i, ii;
     var active_slide = 0;
+    var slide, i, ii, node_name, node, scroller;
 
     // reorganize body to sections if not structured properly
-    if(body.firstElementChild.nodeName !== "SECTION") {
+    if (body.firstElementChild.nodeName !== "SECTION") {
         slide = document.createElement("section");
         slides.push(slide);
         slide.appendChild(body.firstElementChild);
-        while(body.childElementCount > 0) {
+        while (body.childElementCount > 0) {
             node = body.removeChild(body.firstElementChild);
-            nodeName = node.nodeName;
-            if (nodeName === "HR" || nodeName === "H1") {
+            node_name = node.nodeName;
+            if (node_name === "HR" || node_name === "H1") {
                 slide = document.createElement("section");
                 slides.push(slide);
             }
-            if (nodeName !== "HR" && nodeName !== "SCRIPT") {
+            if (node_name !== "HR" && node_name !== "SCRIPT") {
                 slide.appendChild(node);
             }
         }
-        for(i = 0, ii = slides.length; i < ii; ++i) {
+        for (i = 0, ii = slides.length; i < ii; i += 1) {
             body.appendChild(slides[i]);
         }
     } else {
@@ -34,23 +37,32 @@
     }
 
     // add to each slide's object it's active slide index
-    for (i = 0, ii = slides.length; i < ii; ++i) {
+    for (i = 0, ii = slides.length; i < ii; i += 1) {
         slides[i].active_subslide = 0;
     }
 
     if (!window.requestAnimationFrame) {
-	window.requestAnimationFrame = (function() {
+	window.requestAnimationFrame = (function () {
 	    return window.webkitRequestAnimationFrame ||
 		window.mozRequestAnimationFrame ||
 		window.oRequestAnimationFrame ||
 		window.msRequestAnimationFrame ||
-		function(callback, element) {
+		function (callback) {
 		    window.setTimeout( callback, 1000 / 60 );
 		};
-	})();
+	}());
     }
 
-    var vertical_position = function(x) {
+    // general navigational functions
+
+    var update_location = function () {
+        var active_subslide = slides[active_slide].active_subslide;
+        location.assign("#slide-" + (active_slide + 1) +
+                        ((typeof active_subslide === "number" && active_subslide !== 0) ?
+                         ("-" + (active_subslide + 1)) : ""));
+    };
+
+    var vertical_position = function (x) {
         if (typeof x === "undefined") {
             return Math.max(document.documentElement.scrollTop,
                             document.body.scrollTop);
@@ -61,25 +73,24 @@
         }
     };
 
-    var make_scroller = function(target, speed) {
+    var make_scroller = function (target, speed) {
         speed = speed || 10;
-        var begin = vertical_position();
-        var pos;
-        var that = function() {
+        var begin, pos, that;
+        begin = vertical_position();
+        that = function () {
             that.progress = Math.min(that.progress + speed, 100);
             pos = (begin * (100 - that.progress) + (that.target * that.progress)) / 100;
             vertical_position(pos);
             if (that.progress < 100) {
                 requestAnimationFrame(that);
             }
-        }
+        };
         that.target = target;
         that.progress = 0;
         return that;
-    }
+    };
 
-    var scroller;
-    var scroll = function(target){
+    var scroll = function (target) {
         if (scroller && scroller.progress < 100) {
             scroller.target = target;
         } else {
@@ -88,7 +99,7 @@
         }
     };
 
-    var move_to_slide = function(slide_num, immediately) {
+    var move_to_slide = function (slide_num, immediately) {
         if(slide_num < 0 || slide_num >= slides.length) {
             return false;
         }
@@ -97,12 +108,12 @@
         return true;
     };
 
-    Object.prototype.delete_class = function(class_name) {
+    Object.prototype.delete_class = function (class_name) {
         this.className = this.className.replace(class_name, "");
         return this;
-    }
+    };
 
-    var move_to_subslide = function(subslide_num) {
+    var move_to_subslide = function (subslide_num) {
         var slide = slides[active_slide];
         var subs = slide.children;
         var header_offset = subs[0].nodeName === "H1" ? 1 : 0;
@@ -126,7 +137,7 @@
         return true;
     };
 
-    var step = function(direction) {
+    var step = function (direction) {
         var active_subslide = slides[active_slide].active_subslide;
         if(!move_to_subslide(active_subslide + direction)) {
             if(move_to_slide(active_slide + direction)) {
@@ -138,43 +149,41 @@
             }
         }
         update_location();
-    }
+    };
 
     Object.prototype.multiset = function () {
-        for(var i = 0, ii = arguments.length - 1, value = arguments[ii]; i < ii; ++i) {
+        var i, ii, value;
+        for(i = 0, ii = arguments.length - 1, value = arguments[ii]; i < ii; i += 1) {
             this[arguments[i]] = value;
         }
     };
 
+
+    // handling input
     var handlers = {};
+    var zooming = false;
+    var slide_nums = /\#slide-?(\d+)-?(\d*)$/i;
+
     handlers.multiset(38, 33, 37,     function() { step(-1); }); // Up, PageUp and Left
     handlers.multiset(40, 34, 39, 32, function() { step( 1); }); // Down, PageDown, Right and Space
 
-    var preventDefault = function (event) {
-        if (event.preventDefault) event.preventDefault();
+    var prevent_default = function (event) {
+        if (event.preventDefault) { event.preventDefault(); }
         event.returnValue = false;
         return false;
-    }
+    };
 
     window.onkeydown = function (event) {
         var handler = handlers[event.keyCode];
         if(handler){
             handler(event);
-            return preventDefault(event);
+            return prevent_default(event);
         }
     };
 
-    var update_location = function() {
-        var active_subslide = slides[active_slide].active_subslide;
-        location.assign("#slide-" + (active_slide + 1) +
-                        ((typeof active_subslide === "number" && active_subslide !== 0) ?
-                         ("-" + (active_subslide + 1)) : ""));
-    };
-
-    var slide_nums = /\#slide-?(\d+)-?(\d*)$/i;
-    window.onload = function() {
+    window.onload = function () {
         var nums = slide_nums.exec(location.hash);
-        if(nums) {
+        if (nums) {
             move_to_slide((+nums[1])-1, true);
             move_to_subslide((+nums[2])-1);
         } else {
@@ -183,21 +192,21 @@
     };
 
     // Reposition window after zooming
-    window.onresize = function() {
+    window.onresize = function () {
         move_to_slide(active_slide, true);
     };
 
     // Preventing from handling scrolling when Ctrl is pressed --
     // -- otherwise zooming in Firefox 5 is broken
-    var zooming = false;
-    handlers[17] = function() { zooming = true; };
-    window.onkeyup = function(event) {
+    handlers[17] = function () { zooming = true; };
+
+    window.onkeyup = function (event) {
         if (event.keyCode === 17) {
             zooming = false;
         }
-    }
+    };
 
-    var wheel = function(event) {
+    var wheel = function (event) {
         if (!zooming) {
             var delta;
             if (typeof event.wheelDelta === "number") {
@@ -206,7 +215,7 @@
                 delta = event.detail > 0 ? 1 : -1;
             }
             step(delta);
-            return preventDefault(event);
+            return prevent_default(event);
         }
     };
 
